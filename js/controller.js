c9ff2190dea68155dc682b0eecbcb25f964f42f2
socialight21 <vcarloscruz@gmail.com>
2015-03-31 10:44:33 -0700

    working before firebase

--- js/controller.js
+++ js/controller.js
@@ -3,11 +3,16 @@ angular
    .module('dataBindingApp')
    .controller('MainController', MainController);
 
-function MainController() {
+function MainController($timeout) {
    var self = this;
    self.setPiece = setPiece;
    self.checkVertical = checkVertical;
    self.winningLine = "dude";
+   self.winStatement = "Winner!";
+   self.showWinButton = false;
+   self.showWinLine = false;
+   self.clearTheBoard = clearTheBoard;
+   self.clearTheBoardButton = clearTheBoardButton;
 
 
    self.squaresList = [
@@ -23,8 +28,8 @@ function MainController() {
    ]; 
 
    self.wins = [
-       {circle: true, x: false},
-       {circle: true, x: false},
+       {circle: false, x: false},
+       {circle: false, x: false},
        {circle: false, x: false}
    ]; 
 
@@ -33,7 +38,38 @@ function MainController() {
    self.winUrl = "";
 
 
+
    var count = 0;
+
+
+   function finalWin () {
+   		self.showWinButton = true;
+   }
+
+   function clearTheBoardButton() {
+
+   		for (var i = 0; i < 9; i ++) {
+   			self.squaresList[i].circle = false;
+   			self.squaresList[i].x = false;
+
+   		}
+
+   		for (var i = 0; i < 3; i ++) {
+   			self.wins[i].circle = false;
+   			self.wins[i].x = false;
+   			console.log('you cleared ');
+
+   		}
+
+   		self.win = false;
+   		self.winningLine = "";
+   		self.showWinLine = false;
+   		self.winUrl = "";
+   		count = 0;
+   		console.log(self.win);
+   		self.showWinButton = false;
+
+   }
 
    function setPiece (i) {
    		
@@ -54,27 +90,109 @@ function MainController() {
    			
    }
 
+   function addCircleScore () {
+   		self.winStatement = "Winner!";   			
+
+   		if ((self.wins[0].circle == true) && (self.wins[1].circle == true) && (self.wins[2].circle = true)) {
+   			finalWin();
+   			console.log('you got here')
+   		}
+
+   		else if ((self.wins[0].circle == true) && (self.wins[1].circle == true)) {
+   			self.wins[2].circle = true;
+   			$timeout(clearTheBoard,3000);
+   		}
+
+   		else if ((self.wins[0].circle == true) && (self.wins[1].circle == false)) {
+   			self.wins[1].circle = true;
+   			$timeout(clearTheBoard,3000);
+   		}
+
+   		else if (self.wins[0].circle == false) {
+   			self.wins[0].circle =true;
+   			$timeout(clearTheBoard,3000);
+   		}
+   }
+
+    function addXScore () {
+    	 self.winStatement = "Winner!";   			
+
+    	if ((self.wins[0].x == true) && (self.wins[1].x == true) && (self.wins[2].x = true)) {
+   			finalWin();
+   			console.log('you got here')
+
+   		}
+
+   		else if ((self.wins[0].x == true) && (self.wins[1].x == true)) {
+   			self.wins[2].x = true;
+   			$timeout(clearTheBoard,3000);
+   		}
+
+   		else if ((self.wins[0].x == true) && (self.wins[1].x == false)) {
+   			self.wins[1].x = true;
+   			$timeout(clearTheBoard,3000);
+   		}
+
+   		else if (self.wins[0].x == false) {
+   			self.wins[0].x = true;
+   			$timeout(clearTheBoard,3000);
+   		}
+   }
+
+   function clearTheBoard () {
+   		
+
+   		for (var i = 0; i < 9; i ++) {
+   			self.squaresList[i].circle = false;
+   			self.squaresList[i].x = false;
+   			console.log('you are here');
+
+   		}
+
+   		self.win = false;
+   		self.winningLine = "dude";
+   		self.showWinLine = false;
+
+   		self.winUrl = "";
+   		count = 0;
+   		console.log(self.win);
+
+   	// },2000);
+
+
+
+   }
+
+  
+
 
    function checkVertical () {
+   				console.log(count);
+   				
  
 
 				//  horizontal wins
 				if ( (self.squaresList[0].x == true) && (self.squaresList[1].x == true) && (self.squaresList[2].x == true) ) {
 						self.win = ('true');
 						self.winningLine = "win1";
+						self.showWinLine = true;
+						addXScore();
 						
+
 					}
 
 				else if ( (self.squaresList[3].x == true) && (self.squaresList[4].x == true) && (self.squaresList[5].x == true) ) {
 						self.win = ('true');
 						self.winningLine = "win2";
-						
+						self.showWinLine = true;
+						addXScore();
 					}
 
 				else if ( (self.squaresList[6].x == true) && (self.squaresList[7].x == true) && (self.squaresList[8].x == true) ) {
 						self.win = ('true');
 						self.winningLine = "win3";
-						
+						self.showWinLine = true;
+						addXScore();
 					}
 
 				// vertical wins
@@ -82,19 +200,22 @@ function MainController() {
 				else if ( (self.squaresList[0].x == true) && (self.squaresList[3].x == true) && (self.squaresList[6].x == true) ) {
 						self.win = ('true');
 						self.winningLine = "win4";
-						
+						self.showWinLine = true;
+						addXScore();
 					}
 
 				else if ( (self.squaresList[1].x == true) && (self.squaresList[4].x == true) && (self.squaresList[7].x == true) ) {
 						self.win = ('true');
 						self.winningLine = "win5";
-						
+						self.showWinLine = true;
+						addXScore();
 					}
 
 				else if ( (self.squaresList[2].x == true) && (self.squaresList[5].x == true) && (self.squaresList[8].x == true) ) {
 						self.win = ('true');
 						self.winningLine = "win6";
-						
+						self.showWinLine = true;
+						addXScore();
 					}
 
 				// diagonal wins
@@ -102,34 +223,41 @@ function MainController() {
 				else if ( (self.squaresList[2].x == true) && (self.squaresList[4].x == true) && (self.squaresList[6].x == true) ) {
 					self.win = ('true');
 					self.winningLine = "win7";
-					
+					self.showWinLine = true;
+					addXScore();
 				}
 
 				else if ( (self.squaresList[0].x == true) && (self.squaresList[4].x == true) && (self.squaresList[8].x == true) ) {
 					self.win = ('true');
 					self.winningLine = "win8";
-					
+					self.showWinLine = true;
+					addXScore();
 				}
 
 
 				// circle wins
 
-				else if ( (self.squaresList[0].circlecircle == true) && (self.squaresList[1].circlecircle == true) && (self.squaresList[2].circlecircle == true) ) {
+				else if ( (self.squaresList[0].circle == true) && (self.squaresList[1].circle == true) && (self.squaresList[2].circle == true) ) {
 						self.win = ('true');
 						self.winningLine = "win1";
-						
+						self.showWinLine = true;
+						addCircleScore();
 					}
 
-				else if ( (self.squaresList[3].circlecircle == true) && (self.squaresList[4].circlecircle == true) && (self.squaresList[5].circlecircle == true) ) {
-						self.win = ('true');
-						self.winningLine = "win2";
-						
-					}
+
+				else if ( (self.squaresList[3].circle == true) && (self.squaresList[4].circle == true) && (self.squaresList[5].circle == true) ) {
+					console.log('this right here')
+					self.win = ('true');
+					self.winningLine = "win2";
+					self.showWinLine = true;
+					addCircleScore();
+				}
 
 				else if ( (self.squaresList[6].circle == true) && (self.squaresList[7].circle == true) && (self.squaresList[8].circle == true) ) {
 						self.win = ('true');
 						self.winningLine = "win3";
-						
+						self.showWinLine = true;
+						addCircleScore();
 					}
 
 				// vertical wins
@@ -137,19 +265,22 @@ function MainController() {
 				else if ( (self.squaresList[0].circle == true) && (self.squaresList[3].circle == true) && (self.squaresList[6].circle == true) ) {
 						self.win = ('true');
 						self.winningLine = "win4";
-					
+						self.showWinLine = true;
+						addCircleScore();
 					}
 
 				else if ( (self.squaresList[1].circle == true) && (self.squaresList[4].circle == true) && (self.squaresList[7].circle == true) ) {
 						self.win = ('true');
 						self.winningLine = "win5";
-						
+						self.showWinLine = true;
+						addCircleScore();
 					}
 
 				else if ( (self.squaresList[2].circle == true) && (self.squaresList[5].circle == true) && (self.squaresList[8].circle == true) ) {
 						self.win = ('true');
 						self.winningLine = "win6";
-						
+						self.showWinLine = true;
+						addCircleScore();
 					}
 
 				// diagonal wins
@@ -157,14 +288,24 @@ function MainController() {
 				else if ( (self.squaresList[2].circle == true) && (self.squaresList[4].circle == true) && (self.squaresList[6].circle == true) ) {
 					self.win = ('true');
 					self.winningLine = "win7";
-					
+					self.showWinLine = true;
+					addCircleScore();
 				}
 
 				else if ( (self.squaresList[0].circle == true) && (self.squaresList[4].circle == true) && (self.squaresList[8].circle == true) ) {
 					self.win = ('true');
 					self.winningLine = "win8";
-					
+					self.showWinLine = true;
+					addCircleScore();
 				}
+
+				else if ((count == 9) && (self.win == false)) {
+   					self.winStatement = "..Tied!";   			
+   					$timeout(clearTheBoard,3000);
+   					self.win = ('true');
+   				}
+
+				
 
 				// var a = 0;
 
